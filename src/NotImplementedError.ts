7eb934ea83f811b6f6f29eb62c57136f98a7ebcd
W.S.Son <thsdnjstjq1@naver.com>
2023-08-30 17:46:53 +0900

    Version 0.0.4

--- src/NotImplementedError.ts
+++ src/NotImplementedError.ts
@@ -1,4 +1,4 @@
-class NotImplementedError extends Error {
+export default class NotImplementedError extends Error {
   constructor()
   constructor(message: string)
   constructor(message: string | undefined = "The method or operation is not implemented.") {
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { default as ClassNameHelper } from './ClassNameHelper';
 export { default as MathExtension } from './MathExtension';
+export { default as NotImplementedError } from './NotImplementedError';
